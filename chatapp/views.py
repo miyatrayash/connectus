17f73e9e9e5aeb1cc33ea4085bd31fc4a123b086
proquicklearner <miyatrayash@gmail.com>
2021-02-04 19:11:31 +0530

    Added database tables

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from dijango.http import HttpResponse
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("first page")
