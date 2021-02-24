dc017b4364a27c4ea397d399c16dcabc368b108e
ruchita-oza <70565213+ruchita-oza@users.noreply.github.com>
2021-02-24 07:49:01 +0530

    Added templates dir of login in settings.py

--- connectus/settings.py
+++ connectus/settings.py
@@ -57,7 +57,10 @@ ROOT_URLCONF = 'connectus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'loginmodule/templates')
+            ],
         
         'APP_DIRS': True,
         'OPTIONS': {
