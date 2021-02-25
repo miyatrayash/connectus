43e411f3f950aaddda401490ea989f47e7d13961
ruchita-oza <70565213+ruchita-oza@users.noreply.github.com>
2021-02-25 11:10:52 +0530

    added home page (not done)

--- loginmodule/urls.py
+++ loginmodule/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from loginmodule.views import login, auth_view, logout,logged_in, invalid_login
+from loginmodule.views import login, auth_view, logout,logged_in, invalid_login,home_screen
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^logout/$', logout),
     url(r'^logged_in/$', logged_in),
     url(r'^invalid_login/$', invalid_login),
+    url(r'^home_screen/$', home_screen),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
