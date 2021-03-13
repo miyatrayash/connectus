baa31420b64b8290a997f1bb2fcd3ff7e327af26
proquicklearner <miyatrayash@gmail.com>
2021-03-13 20:03:33 +0530

    Added Password Reset

--- connectus/urls.py
+++ connectus/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from chat.views import home
 from loginmodule.views import login,logout,sign_up,home_screen,contact_us
@@ -28,7 +29,22 @@ urlpatterns = [
     path('', home_screen,name="home_screen"),
     path('contact_us/',contact_us,name="contact_us"),
     path('home/',home,name='home'),
-    path('account/',include('chat.urls',namespace='account'))
+    path('account/',include('chat.urls',namespace='account')),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'), 
+        name='password_change_done'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_reset/password_change.html'), 
+        name='password_change'),
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'),
+    name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'),
+    name='password_reset_complete'),
 ]
 
 
