ac55fcd1940540ac633c0f4c9e3b3a746701d351
ruchita-oza <70565213+ruchita-oza@users.noreply.github.com>
2021-03-16 10:37:44 +0530

    Edited home style

--- connectus/settings.py
+++ connectus/settings.py
@@ -133,7 +133,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 STATIC_ROOT= os.path.join(BASE_DIR, 'static_cdn')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 BASE_URL = "http://127.0.0.1:8000"
