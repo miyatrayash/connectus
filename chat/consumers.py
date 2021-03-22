d1339ece31e41d4287349199137419308510de25
proquicklearner <miyatrayash@gmail.com>
2021-03-22 11:10:21 +0530

    public chat done

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,22 @@
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 from django.contrib.auth import get_user_model
-from django.contrib.humanize.templatetags.humanize import naturaltime, naturalday
+from django.contrib.humanize.templatetags.humanize import naturalday
 from django.utils import timezone
 from datetime import datetime
 from channels.db import database_sync_to_async
 from django.core.paginator import Paginator
-from django.core.serializers import serialize
 from django.core.serializers.python import Serializer
 
 from chat.models import PublicChatRoom, PublicRoomChatMessage
 count = 0
 User = get_user_model()
-MSG_TYPE_MESSAGE = 0
-DEFAULT_ROOM_CHAT_MESSAGE_PAGE_SIZE = 10
+
+DEFAULT_ROOM_CHAT_MESSAGE_PAGE_SIZE = 20
+
+MSG_TYPE_MESSAGE = 0  # For standard messages
+MSG_TYPE_CONNECTED_USER_COUNT = 1 # Sending the number of connected users to the chat room
 class PublicChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         global count
@@ -57,6 +60,7 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
             # Leave the room
                 await self.leave_room(content["room"])
             elif command == "get_room_chat_messages":
+                await self.display_progress_bar(True)
                 room = await get_room_or_error(content['room_id'])
                 payload = await get_room_chat_messages(room, content['page_number'])
                 if payload != None:
@@ -64,7 +68,9 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
                     await self.send_messages_payload(payload['messages'], payload['new_page_number'])
                 else:
                     raise ClientError(204,"Something went wrong retrieving the chatroom messages.")
+                await self.display_progress_bar(False)
         except ClientError as e:
+            await self.display_progress_bar(False)
             await self.handle_client_error(e)
 
 
@@ -150,7 +156,15 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
                 "join": str(room.id)
             })
 
-
+            # send the new user count to the room
+            num_connected_users = await  get_num_connected_users(room)
+            await self.channel_layer.group_send(
+            room.group_name,
+            {
+				"type": "connected.user.count",
+				"connected_user_count": num_connected_users,
+			}
+            )
     async def leave_room(self, room_id):
 
         """
@@ -174,6 +188,18 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
             room.group_name,
             self.channel_name,
         )
+
+        # send the new user count to the room
+        num_connected_users = await get_num_connected_users(room)
+        await self.channel_layer.group_send(
+        room.group_name,
+			{
+				"type": "connected.user.count",
+				"connected_user_count": num_connected_users,
+			}
+		)
+
+
 
     async def handle_client_error(self, e):
             """
@@ -199,10 +225,43 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
 			},
 		)
 
+    async def connected_user_count(self, event):
+        """
+		Called to send the number of connected users to the room.
+		This number is displayed in the room so other users know how many users are connected to the chat.
+		"""
+		# Send a message down to the client
+        print("PublicChatConsumer: connected_user_count: count: " + str(event["connected_user_count"]))
+        await self.send_json(
+			{
+				"msg_type": MSG_TYPE_CONNECTED_USER_COUNT,
+				"connected_user_count": event["connected_user_count"]
+			},
+		)
+
+    async def display_progress_bar(self, is_displayed):
+            """
+        1. is_displayed = True
+		- Display the progress bar on UI
+		2. is_displayed = False
+		- Hide the progress bar on UI
+        """
+            print("DISPLAY PROGRESS BAR: " + str(is_displayed))
+            await self.send_json(
+			{
+				"display_progress_bar": is_displayed
+			}
+		)
 def is_authenticated(user):
     if user.is_authenticated:
         return True
     return False
+
+@sync_to_async
+def get_num_connected_users(room):
+	if room.users:
+		return len(room.users.all())
+	return 0
 
 @database_sync_to_async
 def create_public_room_chat_message(room, user, message):
@@ -240,7 +299,7 @@ def get_room_or_error(room_id):
 @database_sync_to_async
 def get_room_chat_messages(room, page_number):
     try:
-        qs = PublicRoomChatMessage.objects.filter(room=room).order_by("timestamp")
+        qs = PublicRoomChatMessage.objects.filter(room=room).order_by("-timestamp")
         p = Paginator(qs, DEFAULT_ROOM_CHAT_MESSAGE_PAGE_SIZE)
         payload = {}
         messages_data = None
@@ -294,12 +353,14 @@ def calculate_timestamp(timestamp):
     return str(ts)
 
 class LazyRoomChatMessageEncoder(Serializer):
-	def get_dump_object(self, obj):
-		dump_object = {}
-		dump_object.update({'msg_type': MSG_TYPE_MESSAGE})
-		dump_object.update({'user_id': str(obj.user.id)})
-		dump_object.update({'username': str(obj.user.username)})
-		dump_object.update({'message': str(obj.content)})
-		dump_object.update({'profile_image': str(obj.user.profile_image.url)})
-		dump_object.update({'natural_timestamp': calculate_timestamp(obj.timestamp)})
-		return dump_object
+	
+    def get_dump_object(self, obj):
+        dump_object = {}
+        dump_object.update({'msg_type': MSG_TYPE_MESSAGE})
+        dump_object.update({'msg_id': str(obj.id)})
+        dump_object.update({'user_id': str(obj.user.id)})
+        dump_object.update({'username': str(obj.user.username)})
+        dump_object.update({'message': str(obj.content)})
+        dump_object.update({'profile_image': str(obj.user.profile_image.url)})
+        dump_object.update({'natural_timestamp': calculate_timestamp(timezone.localtime(obj.timestamp))})
+        return dump_object
